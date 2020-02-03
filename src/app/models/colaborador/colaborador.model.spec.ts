5485be633d19cd41ba1fae587b2f28fdc9a907d8
SousaEdson <edsonsousa734@gmail.com>
2020-02-03 14:25:55 -0300

    Corrigindo estrutura de diretórios

--- src/app/models/colaborador/colaborador.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Colaborador } from './colaborador.model';
-
-describe('Colaborador', () => {
-  it('should create an instance', () => {
-    expect(new Colaborador()).toBeTruthy();
-  });
-});
--- src/app/models/usuario/usuario.model.ts
+++ src/app/models/usuario/usuario.model.ts
@@ -1,4 +1,4 @@
-export class Colaborador {
+export class Usuario {
 
     id: number;
     nome: string;
