49f85d6355f2de1aa3e3909911153bee2b3ef5ea
ycc-blue <yasmin_nim@hotmail.com>
2020-05-14 16:05:15 -0300

    environment e package

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,17 @@
-export const environment = {
-  production: true
-};
+import { environment } from './environment';
+import { OnInit } from '@angular/core';
+
+export class SettingsComponent implements OnInit {
+
+  environmentIsProduction: boolean = false;
+
+  constructor() {
+
+  }
+
+  ngOnInit() {
+    this.environmentIsProduction = environment.production;
+    console.warn('Environment is PRODUCTION: ' + environment.production);
+  }
+
+}
