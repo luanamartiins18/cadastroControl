7c440642745dbfdafdf0070efd8785577c0c317e
ycc-blue <yasmin_nim@hotmail.com>
2020-05-14 16:26:21 -0300

    environment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,17 +1,3 @@
-import { environment } from './environment';
-import { OnInit } from '@angular/core';
-
-export class SettingsComponent implements OnInit {
-
-  environmentIsProduction: boolean = false;
-
-  constructor() {
-
-  }
-
-  ngOnInit() {
-    this.environmentIsProduction = environment.production;
-    console.warn('Environment is PRODUCTION: ' + environment.production);
-  }
-
-}
+export const environment = {
+  production: true
+};
